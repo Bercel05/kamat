4d7822e7f7d9fa2b9658d9d772d306540c44ee5e
MarkusBercel <markusb@kkszki.hu>
2024-10-11 12:19:27 +0200

    first commit

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MarkusBkamat
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Kérem adja meg, hogy hány évre szeretné lekötni a pénzét a bankba");
+            int ev = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine(ev);
+            Console.WriteLine("Kérem adja meg, hogy mekkora összeget szeretne lekötni");
+            double osszeg = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine(osszeg);
+            Console.WriteLine("Mekkora a kamat");
+            double kamat = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine(kamat);
+            double vegosszeg = osszeg * Math.Pow(1 + kamat / 100, ev);
+
+            
+            Console.WriteLine($"{ev} év múlva: {vegosszeg:F2} Ft");
+        }
+    }
+}
